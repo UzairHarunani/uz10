5bef5123af861af1990a8a177283ef5a202198dd
Uz <161607227+UzairHarunani@users.noreply.github.com>
2024-07-14 14:49:25 +0300

    Update todo-list.js

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -13,11 +13,12 @@ for (let i = 0; i < todoList.length; i++) {
   const todoObject = todoList[i];
   //const name = todoObject.name;
   //const dueDate = todoObject.dueDate;
-  const { name, dueDate, dueTime } = todoObject;
+  const { name, dueDate, dueTime, person } = todoObject;
   const html = `
   <div>${name}</div>
   <div>${dueDate}</div>
   <div>${dueTime}</div>
+  <div>${person}</div>
 
   <input type="checkbox">
  
@@ -43,13 +44,17 @@ function addTodo() {
 
   const timeInputElement = document.querySelector('.js-time-input');
   const dueTime = timeInputElement.value;
+
+  const personInputElement = document.querySelector('.js-person-input');
+  const person = personInputElement.value;
  
   todoList.push({
     //name: name,
     //dueDate: dueDate,
     name,
     dueDate,
-    dueTime
+    dueTime,
+    person
       });
   
 localStorage.setItem('todolist', JSON.stringify(todoList));

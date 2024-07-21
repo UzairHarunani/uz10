982a4e0337547bbcb748a65224658bee75ee70e8
Uz <161607227+UzairHarunani@users.noreply.github.com>
2024-07-21 15:46:48 +0300

    Update todo-list.js

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -36,7 +36,7 @@ function addTodo() {
   const name = nameInputElement.value;
 
   const dateInputElement = document.querySelector('.js-date-input');
-  let dueDate = dateInputElement.value;
+  let dueDate = dateInputElement.value.toLocaleDateString();
 
   const timeInputElement = document.querySelector('.js-time-input');
   const dueTime = timeInputElement.value;

0b90018e9758140470ec53acce2d4a615b4e0807
Uzair Harunani <uzair.harunani@student.akest.org>
2024-08-06 18:13:56 +0300

    add offline.html and edit serviceworker and original page

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,58 @@
+const CACHE_NAME = 'your-cache-name-v1';
+const urlsToCache = [
+  '/uz10',
+  '/uz10/azu.html',
+  '/offline.html' // Add offline.html to the cache list
+];
+
+// Install the service worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('my-pwa-cache').then(cache => {
-      return cache.addAll([
-        '/uz10',
-        '/uz10/azu.html',
-        '/uz10/me.jpg',
-        '/uz10/imgs/Rock-emoji.png'
-      ]);
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate the service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
+// Fetch event handler
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          networkResponse => {
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+            const responseToCache = networkResponse.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return networkResponse;
+          }
+        ).catch(() => caches.match('/uz10/offline.html'));
+      })
   );
 });
